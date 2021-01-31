0161342cb35d9c299fe4395911a9b988fe5c93fa
nhieckqo <nhieckqo9@yahoo.com>
2021-01-31 20:46:45 +0800

    added serializers

---
+++ blog/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from .models import Post, Comment
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'emial', 'is_staff']
+
+
+class PostSerializer(serializers.HyperlinkedModelSerializer):
+    user = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Post
+        fields = ['url', 'title', 'user', 'content']
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Comment
+        fields = ['content', 'user']
